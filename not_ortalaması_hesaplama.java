b6fb27b20f9ce9d47c5e59ab1c73972c573abc6b
nisanigarbal <143124801+nisanigarbal@users.noreply.github.com>
2023-09-25 17:22:34 +0300

    not ortalaması hesaplama
    java ile not ortalaması hesabı

---
+++ not_ortalaması_hesaplama.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+class main {
+    public static void main ( String []args) {
+
+        Scanner matematik = new Scanner(System.in);
+        System.out.print("matematik notunu giriniz:");
+        int a = matematik.nextInt();
+
+        Scanner fizik = new Scanner(System.in);
+        System.out.print("fizik  notunu giriniz:");
+        int b = fizik.nextInt();
+
+        Scanner kimya = new Scanner(System.in);
+        System.out.print("kimya  notunu giriniz:");
+        int c = kimya.nextInt();
+
+        Scanner turkce = new Scanner(System.in);
+        System.out.print("turkce notunu giriniz:");
+        int d = turkce.nextInt();
+
+        Scanner tarih = new Scanner(System.in);
+        System.out.print("tarih notunu giriniz:");
+        int e = tarih.nextInt();
+
+        Scanner muzik = new Scanner(System.in);
+        System.out.print("muzik notunu giriniz:");
+        int f = muzik.nextInt();
+
+        double ortalama = ((a+b+c+d+e+f)/6);
+        System.out.print("ortalamanız:" + ortalama );
+
+        String sonuc= ortalama >= 60 ? "sınıfı geçtiniz " : "sınıfta kaldı";
+        System.out.println(sonuc);
+
+
+    }
+}
